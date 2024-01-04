de53b959a64a07986c19ca7e3498906d03b31829
Aisheru <i.am.aisheru@proton.me>
2024-01-04 17:12:51 +0100

    Made the code more pretty

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,7 +4,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
